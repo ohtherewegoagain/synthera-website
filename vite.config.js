549774d0762ab0677d39f32172128cd912ec201c
ohtherewegoagain <pooja892892@gmail.com>
2025-04-15 21:54:58 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // IMPORTANT: Replace this with your actual repo name!
-const repoName = 'synthera-website-main'
+const repoName = 'synthera-website'
 
 export default defineConfig({
-  base: `/${synthera-website}/`,
+  base: `/synthera-website/`,
   plugins: [react()],
 })
